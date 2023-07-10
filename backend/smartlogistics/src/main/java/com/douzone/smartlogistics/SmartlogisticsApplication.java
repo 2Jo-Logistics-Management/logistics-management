6135b32044797379c2094551e20c5b9034873735
bunsung92 <bunsung92@naver.com>
2023-07-10 18:07:43 +0900

    initialize BackEnd Project

---
+++ backend/smartlogistics/src/main/java/com/douzone/smartlogistics/SmartlogisticsApplication.java
@@ -0,0 +1,13 @@
+package com.douzone.smartlogistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartlogisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartlogisticsApplication.class, args);
+	}
+
+}
