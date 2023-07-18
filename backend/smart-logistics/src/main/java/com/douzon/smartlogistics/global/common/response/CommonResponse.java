2d5471ee7fe07e5c083243a49e6bb5640a3ae16c
bunsung92 <bunsung92@naver.com>
2023-07-18 15:24:42 +0900

    feat: 공통 응답 객체 설정 추가 #4

---
+++ backend/smart-logistics/src/main/java/com/douzon/smartlogistics/global/common/response/CommonResponse.java
@@ -0,0 +1,31 @@
+package com.douzon.smartlogistics.global.common.response;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class CommonResponse<T> {
+
+    private final boolean success;
+    private final T data;
+    private final ErrorResponse error;
+
+    /**
+     * 리턴 타입이 Void 일 때 사용
+     */
+    public static CommonResponse<String> successWithDefaultMessage() {
+        return new CommonResponse<>(true, "processing complete!!", null);
+    }
+
+    /**
+     * 성공한 응답에 ResponseBody를 담아 줄 때 사용
+     */
+    public static <T> CommonResponse<T> successWith(T data) {
+        return new CommonResponse<>(true, data, null);
+    }
+
+    public static <T> CommonResponse<T> error(ErrorResponse error) {
+        return new CommonResponse<>(false, null, error);
+    }
+}
