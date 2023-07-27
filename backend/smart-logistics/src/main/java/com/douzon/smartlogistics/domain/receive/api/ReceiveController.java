4e7ec281607fe514d9f971fd5bfefe5c62960f10
Ozini08 <wj940810@gmail.com>
2023-07-27 09:55:03 +0900

    feat : 입고 등록 기능 구현 #10

--- backend/smart-logistics/src/main/java/com/douzon/smartlogistics/domain/receive/api/ReceiveController.java
+++ backend/smart-logistics/src/main/java/com/douzon/smartlogistics/domain/receive/api/ReceiveController.java
@@ -1,22 +1,19 @@
 package com.douzon.smartlogistics.domain.receive.api;
 
-import com.douzon.smartlogistics.domain.entity.Receive;
+import com.douzon.smartlogistics.domain.entity.ReceiveList;
+import com.douzon.smartlogistics.domain.entity.CmpPOrder;
 import com.douzon.smartlogistics.domain.receive.application.ReceiveService;
 import com.douzon.smartlogistics.global.common.response.CommonResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @RestController
@@ -26,23 +23,76 @@ public class ReceiveController {
 
     private final ReceiveService receiveService;
 
-    @GetMapping("/receive-list")
-    public ResponseEntity<CommonResponse<List<Receive>>> findReceive(
+    @GetMapping("/list")
+    public ResponseEntity<CommonResponse<List<ReceiveList>>> findReceive(
             @RequestParam(required = false, defaultValue = "") String receiveCode,
             @RequestParam(required = false, defaultValue = "") String manager,
-            @RequestParam(required = false, defaultValue = "0") int itemCode,
+            @RequestParam(required = false, defaultValue = "0") Integer itemCode,
             @RequestParam(required = false, defaultValue = "") String itemName,
-            @RequestParam(required = false, defaultValue = "0") int accountNo,
+            @RequestParam(required = false, defaultValue = "0") Integer accountNo,
             @RequestParam(required = false, defaultValue = "") String accountName,
             @RequestParam(required = false, defaultValue = "") String startDate,
             @RequestParam(required = false, defaultValue = "") String endDate
     ){
-        List<Receive> receiveList = receiveService.findReceive(
+        List<ReceiveList> receiveList = receiveService.findReceive(
                 receiveCode, manager, itemCode, itemName, accountNo, accountName, startDate, endDate);
 
         return ResponseEntity.status(HttpStatus.OK)
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(CommonResponse.successWith(receiveList));
+    }
+
+    @GetMapping("/insert")
+    public ResponseEntity<CommonResponse<List<CmpPOrder>>> waitingReceive(
+            @RequestParam(required = false, defaultValue = "") String porderCode,
+            @RequestParam(required = false, defaultValue = "0") Integer itemCode,
+            @RequestParam(required = false, defaultValue = "") String itemName,
+            @RequestParam(required = false, defaultValue = "") String manager,
+            @RequestParam(required = false, defaultValue = "0") Integer accountNo,
+            @RequestParam(required = false, defaultValue = "") String accountName,
+            @RequestParam(required = false, defaultValue = "") String startDate,
+            @RequestParam(required = false, defaultValue = "") String endDate
+    ){
+        List<CmpPOrder> cmpPOrderList = receiveService.waitingReceive(
+                porderCode, itemCode, itemName, manager, accountNo, accountName, startDate, endDate
+        );
+        return ResponseEntity.status(HttpStatus.OK)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(CommonResponse.successWith(cmpPOrderList));
+    }
+
+    @PostMapping("/insert")
+    public ResponseEntity<CommonResponse<String>> insertReceive(@RequestBody List<Map<String,Object>> receiveList){
+
+        String createIp = "192.168.0.250";
+        String createId = "admin";
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("createIp", createIp);
+        map.put("createId", createId);
+        map.put("receiveList",receiveList);
+        System.out.println("map = " + map);
+        receiveService.insertReceive(map);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(CommonResponse.successWithDefaultMessage());
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<CommonResponse<String>> deleteReceive(@RequestParam String receiveCode){
+        receiveService.deleteReceive(receiveCode);
+        return ResponseEntity.status(HttpStatus.OK)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(CommonResponse.successWithDefaultMessage());
+    }
+
+    @DeleteMapping("/delete/receive-item")
+    public ResponseEntity<CommonResponse<String>> deleteReceiveItem(@RequestParam Long receiveItemNo) {
+        receiveService.deleteReceiveItem(receiveItemNo);
+        return ResponseEntity.status(HttpStatus.OK)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(CommonResponse.successWithDefaultMessage());
     }
 
 }
--- backend/smart-logistics/src/main/java/com/douzon/smartlogistics/domain/receive/dao/mapper/ReceiveMapper.java
+++ backend/smart-logistics/src/main/java/com/douzon/smartlogistics/domain/receive/dao/mapper/ReceiveMapper.java
@@ -1,21 +1,49 @@
 package com.douzon.smartlogistics.domain.receive.dao.mapper;
 
+import com.douzon.smartlogistics.domain.entity.CmpPOrder;
 import com.douzon.smartlogistics.domain.entity.Receive;
+import com.douzon.smartlogistics.domain.entity.ReceiveItem;
+import com.douzon.smartlogistics.domain.entity.ReceiveList;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @Mapper
 public interface ReceiveMapper {
-    List<Receive> findReceive(
+    List<ReceiveList> findReceive(
             @Param("receiveCode") String receiveCode,
             @Param("manager") String manager,
-            @Param("itemCode") int itemCode,
+            @Param("itemCode") Integer itemCode,
             @Param("itemName") String itemName,
-            @Param("accountNo") int accountNo,
+            @Param("accountNo") Integer accountNo,
             @Param("accountName") String accountName,
             @Param("startDate") String startDate,
             @Param("endDate") String endDate
     );
+
+    List<CmpPOrder> waitingReceive(
+            @Param("porderCode") String porderCode,
+            @Param("itemCode") Integer itemCode,
+            @Param("itemName") String itemName,
+            @Param("manager") String manager,
+            @Param("accountNo") Integer accountNo,
+            @Param("accountName") String accountName,
+            @Param("startDate") String startDate,
+            @Param("endDate") String endDate
+    );
+
+    void insertReceive(Map<String, Object> receive);
+
+    void insertReceiveItem(@Param("receiveItem") List<Map<String, Object>> receiveItem);
+
+    Optional<ReceiveItem> retrieveReceiveItem(Long receiveItemNo);
+
+    Optional<Receive> retrieve(String receiveCode);
+
+    void deleteReceive(String receiveCode);
+
+    void deleteReceiveItem(Long receiveItemNo);
 }
