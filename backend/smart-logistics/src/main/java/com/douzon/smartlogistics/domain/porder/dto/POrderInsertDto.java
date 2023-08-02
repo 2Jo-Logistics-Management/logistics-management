c89a20013360e49fa4087c989404b9a08b0fb88d
bunsung92 <bunsung92@naver.com>
2023-08-02 16:37:37 +0900

    feat: 발주 Swagger 추가 #22

--- backend/smart-logistics/src/main/java/com/douzon/smartlogistics/domain/porder/dto/POrderInsertDto.java
+++ backend/smart-logistics/src/main/java/com/douzon/smartlogistics/domain/porder/dto/POrderInsertDto.java
@@ -2,6 +2,7 @@ package com.douzon.smartlogistics.domain.porder.dto;
 
 import com.douzon.smartlogistics.domain.porderitem.dto.POrderItemInsertDto;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 import javax.validation.constraints.Digits;
 import lombok.Getter;
@@ -16,14 +17,20 @@ import org.springframework.lang.Nullable;
 @ToString
 public class POrderInsertDto {
 
+    @Schema(title = "발주 코드", description = "등록할 발주 코드 입니다.")
     @Nullable
     private String pOrderCode;
+    @Schema(title = "발주 날짜", description = "등록할 발주 날짜 입니다.")
     @Nullable
     private String pOrderDate;
+    @Schema(title = "생성 IP", description = "등록할 발주의 생성 IP 입니다.", example = "발주 테스트 IP")
     private String createIp;
+    @Schema(title = "생성 ID", description = "등록할 발주의 생성 IP 입니다.", example = "발주 테스트 ID")
     private String createId;
+    @Schema(title = "거래처 번호", description = "등록할 거래처 번호 입니다." , example = "1")
     @Digits(integer = 10, fraction = 0)
     private Integer accountNo;
+    @Schema(title = "발주품목 등록 데이터", description = "등록할 발주 품목 리스트 입니다.")
     @JsonProperty(value = "pOrderItems")
     private List<POrderItemInsertDto> pOrderItems;
 }
