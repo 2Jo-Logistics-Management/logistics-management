51024a3eb61ec6075fe9f18706004018c4a9247b
bunsung92 <bunsung92@naver.com>
2023-08-02 16:38:20 +0900

    feat: 발주 물품 Swagger 추가 #22

--- backend/smart-logistics/src/main/java/com/douzon/smartlogistics/global/common/config/SpringFoxConfig.java
+++ backend/smart-logistics/src/main/java/com/douzon/smartlogistics/global/common/config/SpringFoxConfig.java
@@ -13,16 +13,14 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 // http://localhost:8888/swagger-ui/index.html
 @Configuration
-@EnableSwagger2
 public class SpringFoxConfig extends WebMvcConfigurationSupport {
 
     @Bean
     public Docket api(TypeResolver typeResolver) {
-        return new Docket(DocumentationType.SWAGGER_2)
+        return new Docket(DocumentationType.OAS_30)
             .additionalModels(typeResolver.resolve(CommonResponse.class))
             .apiInfo(apiInfo())
             .select()
