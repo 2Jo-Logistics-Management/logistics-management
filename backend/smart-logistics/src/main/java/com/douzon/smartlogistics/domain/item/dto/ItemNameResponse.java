ce9cebf2d9dfa33697a6f9173f32f4583f2ce2f6
kaf829@naver.com <kaf829@naver.com>
2023-08-03 13:23:33 +0900

    Revert "feat: 창고 관리 1차 개발 완료 #14"
    This reverts commit fecf06c66d757c1f297a7802d659a0df45074c19.

--- backend/smart-logistics/src/main/java/com/douzon/smartlogistics/domain/item/dto/ItemNameResponse.java
+++
@@ -1,9 +0,0 @@
-package com.douzon.smartlogistics.domain.item.dto;
-
-import lombok.RequiredArgsConstructor;
-
-@RequiredArgsConstructor
-public class ItemNameResponse {
-
-    private final String itemName;
-}
