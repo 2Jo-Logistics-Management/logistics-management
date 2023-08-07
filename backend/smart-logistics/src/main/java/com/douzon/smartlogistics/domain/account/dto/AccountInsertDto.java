88ba432f51e03ff96c0387543320dfa3015847e7
ap9407 <ap9407@naver.com>
2023-08-07 12:35:08 +0900

    fix: AccoutInsertDto/AccountModifyDto @Scema 어노테이션 적용 #24

--- backend/smart-logistics/src/main/java/com/douzon/smartlogistics/domain/account/dto/AccountInsertDto.java
+++ backend/smart-logistics/src/main/java/com/douzon/smartlogistics/domain/account/dto/AccountInsertDto.java
@@ -1,32 +1,34 @@
 package com.douzon.smartlogistics.domain.account.dto;
 
-import lombok.*;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
-@RequiredArgsConstructor
+@NoArgsConstructor
 @Getter
-@ToString
+@Setter
 public class AccountInsertDto {
-
+    @Schema(title = "거래처명", description = "거래처명 입니다.", example = "테스트 거래처명")
     @NotBlank
     @Size(max = 20)
-    private final String accountName;
+    private String accountName;
+    @Schema(title = "대표자", description = "대표자 입니다.", example = "테스트 대표자")
     @NotBlank
     @Size(max = 10)
-    private final String representative;
-    private final String contactNumber;
-    private final String businessNumber;
-
+    private String representative;
+    @Schema(title = "거래처번호", description = "거래처번호 입니다.", example = "테스트 거래처번호")
+    @Size(max = 30)
+    private String contactNumber;
+    @Schema(title = "사업자번호", description = "사업자번호 입니다.", example = "테스트 사업자번호")
+    @Size(max = 30)
+    private String businessNumber;
+    @Schema(title = "생성 IP", description = "등록할 물품의 생성 IP 입니다.", example = "테스트 IP")
     private String createIp;
+    @Schema(title = "생성 ID", description = "등록할 물품의 생성 IP 입니다.", example = "테스트 ID")
     private String createId;
 
-    public void setCreateIp(String createIp) {
-        this.createIp = createIp;
-    }
-
-    public void setCreateId(String createId) {
-        this.createId = createId;
-    }
 }
