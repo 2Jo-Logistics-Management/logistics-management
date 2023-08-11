733179e6a6f0ef79fe8ca4c1c159e4f3c70a45b2
Ozini08 <wj940810@gmail.com>
2023-08-11 13:23:06 +0900

    feat: 입고 페이징 기능 추가 #45

--- backend/smart-logistics/src/main/java/com/douzon/smartlogistics/domain/receive/api/ReceiveController.java
+++ backend/smart-logistics/src/main/java/com/douzon/smartlogistics/domain/receive/api/ReceiveController.java
@@ -7,6 +7,8 @@ import com.douzon.smartlogistics.domain.receive.dto.ReceiveInsertDto;
 import com.douzon.smartlogistics.domain.receive.dto.ReceiveModifyDto;
 import com.douzon.smartlogistics.global.common.aop.annotation.TimeOut;
 import com.douzon.smartlogistics.global.common.response.CommonResponse;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import io.swagger.annotations.Api;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -39,7 +41,7 @@ public class ReceiveController {
                     ))})
     @TimeOut
     @GetMapping("/list")
-    public ResponseEntity<CommonResponse<List<ReceiveListDto>>> findReceive(
+    public ResponseEntity<CommonResponse<PageInfo<ReceiveListDto>>> findReceive(
             @RequestParam(required = false, defaultValue = "") @Parameter(description = "입고코드") String receiveCode,
             @RequestParam(required = false, defaultValue = "") @Parameter(description = "담당자") String manager,
             @RequestParam(required = false, defaultValue = "0") @Parameter(description = "상품코드") Integer itemCode,
@@ -47,15 +49,21 @@ public class ReceiveController {
             @RequestParam(required = false, defaultValue = "0") @Parameter(description = "거래처번호") Integer accountNo,
             @RequestParam(required = false, defaultValue = "") @Parameter(description = "거래처명") String accountName,
             @RequestParam(required = false, defaultValue = "") @Parameter(description = "조회 시작날짜") String startDate,
-            @RequestParam(required = false, defaultValue = "") @Parameter(description = "조회 마감날짜") String endDate
+            @RequestParam(required = false, defaultValue = "") @Parameter(description = "조회 마감날짜") String endDate,
+            @RequestParam(defaultValue = "1") @Parameter(description = "페이지 번호") int pageNum,
+            @RequestParam(defaultValue = "50") @Parameter(description = "페이지 크기") int pageSize
     ) {
         log.info("ReceiveControllerListIN");
+        PageHelper.startPage(pageNum, pageSize);
+
         List<ReceiveListDto> receiveListDto = receiveService.findReceive(
                 receiveCode, manager, itemCode, itemName, accountNo, accountName, startDate, endDate);
+        PageInfo<ReceiveListDto> pageInfo = new PageInfo<>(receiveListDto);
+
         log.info("ReceiveControllerListOUT");
         return ResponseEntity.status(HttpStatus.OK)
                 .contentType(MediaType.APPLICATION_JSON)
-                .body(CommonResponse.successWith(receiveListDto));
+                .body(CommonResponse.successWith(pageInfo));
     }
     @Operation(summary = "입고 등록 전 발주 완료 리스트 조회",
             description = "입고 등록 전 발주 완료 리스트 조회 요청을 처리하고 데이터베이스를 조회해 리스트로 결과를 반환합니다.",
