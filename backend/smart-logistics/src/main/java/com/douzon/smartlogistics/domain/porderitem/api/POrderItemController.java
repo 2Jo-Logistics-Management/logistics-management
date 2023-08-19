8c2c1d5f44039a5de3f867134d7499a815ef3b26
bunsung92 <bunsung92@naver.com>
2023-08-19 23:21:28 +0900

    feat: 발주물품 상태 수정 api 추가 #47

--- backend/smart-logistics/src/main/java/com/douzon/smartlogistics/domain/porderitem/api/POrderItemController.java
+++ backend/smart-logistics/src/main/java/com/douzon/smartlogistics/domain/porderitem/api/POrderItemController.java
@@ -4,6 +4,7 @@ import com.douzon.smartlogistics.domain.entity.POrderItem;
 import com.douzon.smartlogistics.domain.porderitem.application.POrderItemService;
 import com.douzon.smartlogistics.domain.porderitem.dto.POrderItemInsertDto;
 import com.douzon.smartlogistics.domain.porderitem.dto.POrderItemModifyDto;
+import com.douzon.smartlogistics.domain.porderitem.dto.POrderItemStateModifyDto;
 import com.douzon.smartlogistics.global.common.response.CommonResponse;
 import io.swagger.annotations.Api;
 import io.swagger.v3.oas.annotations.Operation;
@@ -20,6 +21,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,14 +42,14 @@ public class POrderItemController {
                                         content = @Content(mediaType = "application/json",
                                                            schema = @Schema(implementation = CommonResponse.class))))
     @PostMapping("/insert")
-    public ResponseEntity<CommonResponse<String>> insert(@RequestBody @Valid @Parameter(description = "발주품목 등록을 위한 "
+    public ResponseEntity<CommonResponse<Integer>> insert(@RequestBody @Valid @Parameter(description = "발주품목 등록을 위한 "
         + "데이터") POrderItemInsertDto pOrderItemInsertDto) {
 
-        pOrderItemService.insert(pOrderItemInsertDto);
+        Integer createdPOrderItemNo = pOrderItemService.insert(pOrderItemInsertDto);
 
         return ResponseEntity.status(HttpStatus.CREATED)
                              .contentType(MediaType.APPLICATION_JSON)
-                             .body(CommonResponse.successWithDefaultMessage());
+                             .body(CommonResponse.successWith(createdPOrderItemNo));
     }
 
     @Operation(summary = "발주물품 수정",
@@ -56,15 +58,31 @@ public class POrderItemController {
                                         content = @Content(mediaType = "application/json",
                                                            schema = @Schema(implementation = CommonResponse.class))))
     @PatchMapping("/modify")
-    public ResponseEntity<CommonResponse<String>> modify(
-        @RequestParam @Parameter(description = "발주물품 번호") Long pOrderItemNo,
-        @RequestBody @Valid @Parameter (description = "발주물품 수정을 위한 데이터") POrderItemModifyDto pOrderItemModifyDto) {
+    public ResponseEntity<CommonResponse<Integer>> modify(
+        @RequestParam @Parameter(description = "발주물품 번호") Integer pOrderItemNo,
+        @RequestBody @Valid @Parameter(description = "발주물품 수정을 위한 데이터") POrderItemModifyDto pOrderItemModifyDto) {
 
         pOrderItemService.modify(pOrderItemNo, pOrderItemModifyDto);
 
         return ResponseEntity.status(HttpStatus.OK)
                              .contentType(MediaType.APPLICATION_JSON)
-                             .body(CommonResponse.successWithDefaultMessage());
+                             .body(CommonResponse.successWith(pOrderItemNo));
+    }
+
+    @Operation(summary = "발주물품 상태 수정",
+               description = "발주물품 상태를 알맞은 데이터를 받아 데이터베이스를 수정합니다.",
+               responses = @ApiResponse(responseCode = "200",
+                                        content = @Content(mediaType = "application/json",
+                                                           schema = @Schema(implementation = CommonResponse.class))))
+    @PatchMapping("/modify/{pOrderItemNo}")
+    public ResponseEntity<CommonResponse<Integer>> modifyState(@PathVariable Integer pOrderItemNo,
+        @RequestBody @Valid @Parameter(description = "발주물품 상태 수정을 위한 데이터") POrderItemStateModifyDto pOrderItemStateModifyDto) {
+
+        Integer modifiedPOrderItemNo = pOrderItemService.modifyState(pOrderItemNo, pOrderItemStateModifyDto);
+
+        return ResponseEntity.status(HttpStatus.OK)
+                             .contentType(MediaType.APPLICATION_JSON)
+                             .body(CommonResponse.successWith(modifiedPOrderItemNo));
     }
 
     @Operation(summary = "발주물품 리스트 조회",
@@ -73,7 +91,8 @@ public class POrderItemController {
                                          content = @Content(mediaType = "application/json", schema = @Schema(implementation = CommonResponse.class)
                                          ))})
     @GetMapping("/list")
-    public ResponseEntity<CommonResponse<List<POrderItem>>> searchPOrderItemList(@RequestParam @Parameter(description = "발주코드") String pOrderCode) {
+    public ResponseEntity<CommonResponse<List<POrderItem>>> searchPOrderItemList(
+        @RequestParam @Parameter(description = "발주코드") String pOrderCode) {
 
         List<POrderItem> pOrderItems = pOrderItemService.searchPOrderItemList(pOrderCode);
 
@@ -86,8 +105,11 @@ public class POrderItemController {
                description = "발주물품 삭제에 알맞은 데이터를 받아 데이터베이스의 데이터를 삭제합니다.",
                responses = @ApiResponse(responseCode = "200"))
     @DeleteMapping("/delete")
-    public ResponseEntity<CommonResponse<String>> delete(@RequestParam @Parameter(description = "발주물품번호") Long pOrderItemNo) {
-        pOrderItemService.delete(pOrderItemNo);
+    public ResponseEntity<CommonResponse<String>> delete(
+        @RequestParam @Parameter(description = "발주물품번호") Integer pOrderItemNo,
+        @RequestParam @Parameter(description = "발주코드") String pOrderCode) {
+
+        pOrderItemService.delete(pOrderItemNo, pOrderCode);
 
         return ResponseEntity.status(HttpStatus.OK)
                              .contentType(MediaType.APPLICATION_JSON)
