607db8c9c5e5bfaccfe889fcbe1368c7c133d986
bunsung92 <bunsung92@naver.com>
2023-08-19 23:11:21 +0900

    refactor: 발주, 발주물품 엔티티 리팩터링 #47

--- backend/smart-logistics/src/main/java/com/douzon/smartlogistics/domain/entity/POrderItem.java
+++ backend/smart-logistics/src/main/java/com/douzon/smartlogistics/domain/entity/POrderItem.java
@@ -2,23 +2,25 @@ package com.douzon.smartlogistics.domain.entity;
 
 import com.douzon.smartlogistics.domain.entity.constant.State;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@RequiredArgsConstructor
+@NoArgsConstructor
+@Setter
 @Getter
 public class POrderItem {
 
-    private final Long pOrderItemNo;
-    private final String pOrderCode;
-    private final Integer itemCode;
-    private final Double pOrderCount;
-    private final Integer pOrderPrice;
-    private final Long pOrderItemPrice;
-    private final State pOrderState;
-    private final String receiveDeadline;
-    private final String createIp;
-    private final String createId;
-    private final String modifyDate;
-    private final String modifyIp;
-    private final String modifyId;
+    private Integer pOrderItemNo;
+    private String pOrderCode;
+    private Integer itemCode;
+    private Double pOrderCount;
+    private Integer pOrderPrice;
+    private Long pOrderItemPrice;
+    private State pOrderState;
+    private String receiveDeadline;
+    private String createIp;
+    private String createId;
+    private String modifyDate;
+    private String modifyIp;
+    private String modifyId;
 }
