611cc42f3cf13905efdcfa3c77b4389327b98750
bunsung92 <bunsung92@naver.com>
2023-08-22 17:03:59 +0900

    refactor: 창고 구역 테이블 컬럼 추가로 인한 엔티티 수정 #51

--- backend/smart-logistics/src/main/java/com/douzon/smartlogistics/domain/entity/WarehouseSection.java
+++ backend/smart-logistics/src/main/java/com/douzon/smartlogistics/domain/entity/WarehouseSection.java
@@ -6,8 +6,13 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Getter
 public class WarehouseSection {
+
     private final Integer sectionNo;
     private final String sectionName;
-
-
+    private final String createDate;
+    private final String createIp;
+    private final String createId;
+    private final String modifyDate;
+    private final String modifyIp;
+    private final String modifyId;
 }
