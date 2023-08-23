74ab2faaabfaa60e72902a0d252f190e0f8b5964
bunsung92 <bunsung92@naver.com>
2023-08-23 15:16:35 +0900

    feat: 테이블 이름 및 구조 변경으로 인한 엔티티 수정 #51

---
+++ backend/smart-logistics/src/main/java/com/douzon/smartlogistics/domain/entity/WarehouseStock.java
@@ -0,0 +1,26 @@
+package com.douzon.smartlogistics.domain.entity;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Date;
+
+@RequiredArgsConstructor
+@Getter
+public class WarehouseStock {
+
+    public final Integer warehouseNo;
+    public final Integer sectionNo;
+    public final String receiveCode;
+    public final String receiveItemCode;
+    public final String itemCode;
+    public final double count;
+    public final Date registDate;
+    public final Date createDate;
+    public final String createId;
+    public final String createIp;
+    public final Date modifyDate;
+    public final String modifyIp;
+    public final String modifyId;
+
+}
