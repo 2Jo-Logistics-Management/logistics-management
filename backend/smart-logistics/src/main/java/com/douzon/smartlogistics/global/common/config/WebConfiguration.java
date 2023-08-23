f657e88bc9adfef472d4820d102ede2d775f937c
ap9407 <ap9407@naver.com>
2023-08-23 10:51:13 +0900

    #feat: back-end 와 fron-end 세션을 처리하기 위해서 추가해줘야할 코드 #7

--- backend/smart-logistics/src/main/java/com/douzon/smartlogistics/global/common/config/WebConfiguration.java
+++ backend/smart-logistics/src/main/java/com/douzon/smartlogistics/global/common/config/WebConfiguration.java
@@ -1,8 +1,11 @@
 package com.douzon.smartlogistics.global.common.config;
 
+import com.douzon.smartlogistics.global.common.config.interceptor.SessionCheckInterceptor;
 import com.douzon.smartlogistics.global.common.converter.StringToStateConverter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -12,4 +15,22 @@ public class WebConfiguration implements WebMvcConfigurer {
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverter(new StringToStateConverter());
     }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true)
+                .allowedHeaders("*");
+    }
+
+    @Override
+    public  void addInterceptors(InterceptorRegistry registry){
+        registry.addInterceptor(new SessionCheckInterceptor())
+                .addPathPatterns("/**");
+    }
+
+
+
 }
